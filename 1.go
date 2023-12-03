c0ac3baa701415c8b6f7f8c32e472c51206c3881
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2023-12-03 23:13:05 +0100

    d1: Rewrite from scratch
    Overlapping numeric words need to also work,
    which seems very silly when you think of them as
    substitutions, but oh well...
    For example, the line 'oneight' (notice only one 'e'!)
    needs to result in the digits 18, instead of it becoming
    '1ight' which would result in the number 11 for that line.

--- 1.go
+++ 1.go
@@ -4,13 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
-func getNumberToDigit() [9]string {
-	return [9]string{
+func getNumberToDigit() []string {
+	return []string{
 		"one",
 		"two",
 		"three",
@@ -23,59 +23,97 @@ func getNumberToDigit() [9]string {
 	}
 }
 
+const noIndex = -1
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var sum int
 	for scanner.Scan() {
-		sum += calculateCalibrationValue(replaceNumberWordWithDigit(scanner.Text()))
+		line := scanner.Text()
+		firstDigit, lastDigit := 0, 0
+		indexOfFirstDigit, indexOfLastDigit := getIndicesOfDigits(line)
+		// "word" meaning numbers like "one", "two" etc. up to "nine"
+		sliceOfFirstWord := getSliceOfFirstWord(line)
+		sliceOfLastWord := getSliceOfLastWord(line)
+
+		// fmt.Println(line)
+		// fmt.Println("indexOfFirstDigit: " + strconv.Itoa(indexOfFirstDigit))
+		// fmt.Println("indexOfLastDigit: " + strconv.Itoa(indexOfLastDigit))
+		// fmt.Printf("indexOfFirstWord: %v\n", sliceOfFirstWord)
+		// fmt.Printf("indexOfLastWord: %v\n", sliceOfLastWord)
+
+		if indexOfFirstDigit == noIndex && sliceOfFirstWord[0] == noIndex {
+			continue
+		}
+
+		if indexOfFirstDigit == noIndex || (sliceOfFirstWord[0] != noIndex && sliceOfFirstWord[0] < indexOfFirstDigit) {
+			firstDigit = getDigitByWordIndex(line, sliceOfFirstWord)
+		} else if sliceOfFirstWord[0] == noIndex || (indexOfFirstDigit != noIndex && indexOfFirstDigit < sliceOfFirstWord[0]) {
+			firstDigit, _ = strconv.Atoi(string(line[indexOfFirstDigit]))
+		}
+
+		if indexOfLastDigit == noIndex || (sliceOfLastWord[0] != noIndex && sliceOfLastWord[0] > indexOfLastDigit) {
+			lastDigit = getDigitByWordIndex(line, sliceOfLastWord)
+		} else if sliceOfLastWord[0] == noIndex || (indexOfLastDigit != noIndex && indexOfLastDigit > sliceOfLastWord[0]) {
+			lastDigit, _ = strconv.Atoi(string(line[indexOfLastDigit]))
+		}
+		// fmt.Println(firstDigit*10 + lastDigit)
+
+		sum += firstDigit*10 + lastDigit
 	}
+
+	fmt.Println("Result:")
 	fmt.Println(strconv.Itoa(sum))
 }
 
-func replaceNumberWordWithDigit(line string) string {
-	// fmt.Println(line)
-	hasNumber := true
-	for hasNumber {
-		firstIndexOfNumber := -1
-		var keyOfFirstNumber int
-		numberToDigit := getNumberToDigit()
-		for key, word := range numberToDigit {
-			index := strings.Index(line, word)
-			if index == -1 {
-				continue
-			}
-			if firstIndexOfNumber == -1 || index < firstIndexOfNumber {
-				firstIndexOfNumber = index
-				keyOfFirstNumber = key
-			}
-		}
-		if firstIndexOfNumber != -1 {
-			line = strings.ReplaceAll(line, numberToDigit[keyOfFirstNumber], strconv.Itoa(keyOfFirstNumber+1))
-		} else {
-			hasNumber = false
+func getDigitByWordIndex(line string, slice []int) int {
+	numberFromLine := line[slice[0]:slice[1]]
+	for idx, number := range getNumberToDigit() {
+		if numberFromLine == number {
+			return idx + 1
 		}
 	}
-
-	return line
+	return 0
 }
 
-func calculateCalibrationValue(line string) int {
-	var first, last int
-	digitFound := false
-	for _, char := range line {
-		if unicode.IsDigit(char) {
-			last = int(char - '0') // Convert rune to int
-			if !digitFound {
-				digitFound = true
-				first = int(char - '0') // Convert rune to int
-			}
-		}
+func getIndicesOfDigits(line string) (int, int) {
+	firstIndex, lastIndex := noIndex, noIndex
+	re := regexp.MustCompile(`[[:digit:]]`)
+	matches := re.FindAllStringIndex(line, -1)
+	if len(matches) == 0 {
+		return firstIndex, lastIndex
 	}
+	firstIndex = matches[0][0]
+	lastIndex = matches[len(matches)-1][0]
 
-	sum := first*10 + last
-	// fmt.Println(line)
-	// fmt.Println(strconv.Itoa(first))
-	// fmt.Println(strconv.Itoa(last))
-	// fmt.Println(strconv.Itoa(sum))
-	return sum
+	return firstIndex, lastIndex
+}
+
+func getSliceOfFirstWord(line string) []int {
+	re := regexp.MustCompile(strings.Join(getNumberToDigit(), "|"))
+	matches := re.FindStringIndex(line)
+	if len(matches) == 0 {
+		return []int{noIndex, noIndex}
+	}
+	return matches
+}
+
+func getSliceOfLastWord(line string) []int {
+	re := regexp.MustCompile(Reverse(strings.Join(getNumberToDigit(), "|")))
+	matches := re.FindStringIndex(Reverse(line))
+	if len(matches) == 0 {
+		return []int{noIndex, noIndex}
+	}
+	return []int{
+		(len(line) - 1) - (matches[1] - 1),
+		(len(line) - 1) - matches[0] + 1,
+	}
+}
+
+// https://stackoverflow.com/a/4965535
+func Reverse(s string) (result string) {
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return
 }
