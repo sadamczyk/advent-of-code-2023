7c2f99f17062a4db88f9dae0a73d2a2864b0d72d
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2023-12-05 20:36:36 +0100

    D2-2: pzez

--- 2.go
+++ 2.go
@@ -10,10 +10,22 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	sum := 0
-lines:
+	maxAllowedCounts := map[string]int{
+		"blue":  14,
+		"green": 13,
+		"red":   12,
+	}
+	sumOfIds := 0
+	totalPower := 0
 	for scanner.Scan() {
+		skipId := false
 		line := scanner.Text()
+		// Initialize each color with 1 as we're multiplying these values later
+		maxCountsInGame := map[string]int{
+			"blue":  1,
+			"green": 1,
+			"red":   1,
+		}
 		slices := strings.Split(line, ": ")
 		id, _ := strconv.Atoi(strings.Split(slices[0], " ")[1])
 		for _, subset := range strings.Split(slices[1], "; ") {
@@ -21,18 +33,28 @@ lines:
 				setSlices := strings.Split(set, " ")
 				count, _ := strconv.Atoi(setSlices[0])
 				color := setSlices[1]
-				maxCounts := map[string]int{
-					"blue":  14,
-					"green": 13,
-					"red":   12,
-				}
-
-				if count > maxCounts[color] {
-					continue lines
+				if count > maxCountsInGame[color] {
+					maxCountsInGame[color] = count
 				}
 			}
 		}
-		sum += id
+
+		power := 1
+		for color, count := range maxCountsInGame {
+			power *= count
+
+			if count > maxAllowedCounts[color] {
+				skipId = true
+			}
+		}
+		totalPower += power
+
+		if !skipId {
+			sumOfIds += id
+		}
 	}
-	fmt.Println(sum)
+	fmt.Println("Sum of IDs:")
+	fmt.Println(sumOfIds)
+	fmt.Println("Total power:")
+	fmt.Println(totalPower)
 }
